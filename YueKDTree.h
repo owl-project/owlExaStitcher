01f2db6782d5b3cb47218433f3959784d7aead6c
Stefan Zellmann <info@szellmann.de>
2023-02-15 08:38:30 +0100

    Builder does not need to be recursive anymore

--- YueKDTree.h
+++ YueKDTree.h
@@ -43,7 +43,7 @@ struct KDTree
   KDTree(Volume vol, const std::vector<float> *cm = nullptr);
 
   template <typename Volume>
-  void buildRec(Volume vol);
+  void build(Volume vol);
 
   /*! an optional RGBA colormap that can be used for classification
     (4-tuples for convenience, only the alpha components are relevant) */
@@ -74,98 +74,96 @@ KDTree::KDTree(Volume vol, const std::vector<float> *cm)
 {
   box3f V = vol.getBounds();
   nodes.push({V,FLT_MAX});
-  buildRec(vol);
+  build(vol);
 }
 
 template <typename Volume>
-void KDTree::buildRec(Volume vol) {
-  if (nodes.size() >= NumLeavesDesired)
-    return;
+void KDTree::build(Volume vol) {
+  while (nodes.size() < NumLeavesDesired) {
 
-  std::cout << "Leaf nodes generated so far: " << nodes.size() << ", picking another node to split...\n";
+    std::cout << "Leaf nodes generated so far: " << nodes.size() << ", picking another node to split...\n";
 
-  auto cpy_queue = nodes;
-  float minPriority=1e31f, maxPriority=-1e31f;
-  while (!cpy_queue.empty()) {
-    Node n = cpy_queue.top();
-    cpy_queue.pop();
-    minPriority = fminf(minPriority,n.priority);
-    maxPriority = fmaxf(maxPriority,n.priority);
-  }
+    // auto cpy_queue = nodes;
+    // float minPriority=1e31f, maxPriority=-1e31f;
+    // while (!cpy_queue.empty()) {
+    //   Node n = cpy_queue.top();
+    //   cpy_queue.pop();
+    //   minPriority = fminf(minPriority,n.priority);
+    //   maxPriority = fmaxf(maxPriority,n.priority);
+    // }
 
-  std::cout << "Cost range of nodes in queue: [" << minPriority << ',' << maxPriority << "]\n";
+    // std::cout << "Cost range of nodes in queue: [" << minPriority << ',' << maxPriority << "]\n";
 
-  Node node = nodes.top();
-  nodes.pop();
-  box3f V = node.domain;
-  std::cout << "Picking node: " << V << " with costs " << node.priority << '\n';
+    Node node = nodes.top();
+    nodes.pop();
+    box3f V = node.domain;
+    std::cout << "Picking node: " << V << " with costs " << node.priority << '\n';
 
-  struct Costs { float left, right, total; };
+    struct Costs { float left, right, total; };
 
-  int bestAxis = -1;
-  float bestPlane;
-  Costs bestCost = {FLT_MAX,FLT_MAX,FLT_MAX};
-  for (int axis=0; axis<=2; ++axis) {
-    float begin = V.lower[axis];
-    float end   = V.upper[axis];
+    int bestAxis = -1;
+    float bestPlane;
+    Costs bestCost = {FLT_MAX,FLT_MAX,FLT_MAX};
+    for (int axis=0; axis<=2; ++axis) {
+      float begin = V.lower[axis];
+      float end   = V.upper[axis];
 
-    if (begin == end)
-      continue;
-    float step  = (end-begin)/NumBins;
+      if (begin == end)
+        continue;
+      float step  = (end-begin)/NumBins;
 
-    for (int i=0; i<NumBins-1; ++i) {
-      float plane = begin+step*(i+1);
+      for (int i=0; i<NumBins-1; ++i) {
+        float plane = begin+step*(i+1);
 
-      box3f L = V;
-      box3f R = V;
+        box3f L = V;
+        box3f R = V;
 
-      L.lower[axis] = begin;
-      L.upper[axis] = plane;
+        L.lower[axis] = begin;
+        L.upper[axis] = plane;
 
-      R.lower[axis] = plane;
-      R.upper[axis] = end;
+        R.lower[axis] = plane;
+        R.upper[axis] = end;
 
-      float minValueL=1e31f, maxValueL=-1e31f;
-      float minValueR=1e31f, maxValueR=-1e31f;
-    
-      vol.min_max(L,minValueL,maxValueL,rgbaCM);
-      vol.min_max(R,minValueR,maxValueR,rgbaCM);
-      //minValueL=minValueR=maxValueL=maxValueR=1.f;
+        float minValueL=1e31f, maxValueL=-1e31f;
+        float minValueR=1e31f, maxValueR=-1e31f;
+      
+        vol.min_max(L,minValueL,maxValueL,rgbaCM);
+        vol.min_max(R,minValueR,maxValueR,rgbaCM);
+        //minValueL=minValueR=maxValueL=maxValueR=1.f;
 
-      float CL = surface_area(L) * diag(L) * maxValueL;
-      float CR = surface_area(R) * diag(R) * maxValueR;
-      float C = CL+CR;
+        float CL = surface_area(L) * diag(L) * maxValueL;
+        float CR = surface_area(R) * diag(R) * maxValueR;
+        float C = CL+CR;
 
-      std::cout << "Testing axis " << axis << ", plane " << plane
-                << ", muL: " << maxValueL << ", muR: " << maxValueR
-                << ", CL: " << CL << ", CR: " << CR << ", C: " << C << '\n';
+        std::cout << "Testing axis " << axis << ", plane " << plane
+                  << ", muL: " << maxValueL << ", muR: " << maxValueR
+                  << ", CL: " << CL << ", CR: " << CR << ", C: " << C << '\n';
 
-      if (C < bestCost.total) {
-        bestAxis = axis;
-        bestPlane = begin+step*(i+1);
-        bestCost = {CL,CR,C};
+        if (C < bestCost.total) {
+          bestAxis = axis;
+          bestPlane = begin+step*(i+1);
+          bestCost = {CL,CR,C};
+        }
       }
     }
+
+    float CL = bestCost.left;
+    float CR = bestCost.right;
+    float C  = bestCost.total;
+
+    std::cout << V << ", split at: (" << bestAxis << ',' << bestPlane << ")\n"
+              << "SAH costs(L): " << CL
+              << ", SAH costs(R): " << CR
+              << " (sum: " << C << ")\n\n";
+
+    box3f L = V;
+    L.upper[bestAxis] = bestPlane;
+    nodes.push({L,CL});
+
+    box3f R = V;
+    R.lower[bestAxis] = bestPlane;
+    nodes.push({R,CR});
   }
-
-  float CL = bestCost.left;
-  float CR = bestCost.right;
-  float C  = bestCost.total;
-
-  std::cout << V << ", split at: (" << bestAxis << ',' << bestPlane << ")\n"
-            << "SAH costs(L): " << CL
-            << ", SAH costs(R): " << CR
-            << " (sum: " << C << ")\n\n";
-
-  box3f L = V;
-  L.upper[bestAxis] = bestPlane;
-  nodes.push({L,CL});
-
-  box3f R = V;
-  R.lower[bestAxis] = bestPlane;
-  nodes.push({R,CR});
-
-  buildRec(vol);
 }
 
 } // namespace volkd
